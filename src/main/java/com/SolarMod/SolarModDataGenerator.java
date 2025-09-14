2c687bc319c86b60f623e38b286c96666050633e
Solareddd <ofdeviced@gmail.com>
2025-09-13 19:57:03 -0700

    Watching tutorial, 13:38 in, created mod data generator

--- src/main/java/com/SolarMod/SolarModDataGenerator.java
+++ src/main/java/com/SolarMod/SolarModDataGenerator.java
@@ -1,4 +1,20 @@
 package com.SolarMod;
 
-public class SolarModDataGenerator {
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
+
+public class SolarModDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+
+    }
+
+    @Override
+    public void buildRegistry(RegistryBuilder registryBuilder) {
+
+    }
 }
